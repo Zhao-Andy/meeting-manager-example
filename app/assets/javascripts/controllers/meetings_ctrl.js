e3f3a2622e00740928b173f5b5f742082e994a62
Andy Zhao <andyzhao.zhao@gmail.com>
2016-09-11 14:19:39 -0400

    added angular features

--- app/assets/javascripts/controllers/meetings_ctrl.js
+++ app/assets/javascripts/controllers/meetings_ctrl.js
@@ -2,11 +2,15 @@
   "use strict";
 
   angular.module("app").controller("meetingsCtrl", function($scope, $http) {
+
     $scope.setup = function() {
       $http.get('/api/v1/meetings.json').then(function(response) {
         $scope.meetings = response.data;
       });
-    }
-  } );
+    };
 
+    $scope.sortByDate = function() {
+      $scope.isReverse = !$scope.isReverse;
+      };
+    });
 }());
